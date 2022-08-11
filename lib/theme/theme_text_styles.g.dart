9a40cc94c0c3e84330d58db91eedbfda93fc4ba4
Parabeac-Nest <admin@parabeac.com>
2022-08-11 21:20:16 +0000

    Auto-generated commit by Parabeac Nest

---
+++ lib/theme/theme_text_styles.g.dart
@@ -0,0 +1,30 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class ThemeTextStyles {
+
+static const TextStyle salsaText = TextStyle(
+  fontSize: 30,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Salsa',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle boldItalic = TextStyle(
+  fontSize: 36,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0,
+  fontFamily: 'Libre Bodoni',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.italic,
+)
+;
+}
