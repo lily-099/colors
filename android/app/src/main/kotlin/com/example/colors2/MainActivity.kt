3183082c7a07e001e67a7a2ac3d41726d4a4ea4c
Parabeac-Nest <admin@parabeac.com>
2022-09-05 07:25:18 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ android/app/src/main/kotlin/com/example/colors2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.colors2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/theme/colors2_theme.g.dart
@@ -0,0 +1,15 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';import 'package:colors2/theme/colors2_text_styles.g.dart';
+
+class Colors2Theme {
+
+static const TextTheme textTheme = TextTheme(headline2: Colors2TextStyles.headline2,);
+
+static  ThemeData themeData = ThemeData(textTheme: textTheme,);
+}
